97f9f3738d3fefceb55dd812c018157f55cdde8c
Kroh Nickolay <375336623761@ya.ru>
2020-12-26 19:02:34 +0300

    refactor: remove webpack import in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
