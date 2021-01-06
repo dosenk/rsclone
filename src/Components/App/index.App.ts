4ffe6ea8dbcf0f5847cd9c6f8248b65a5c87f038
Vladislav Gribkov <vladislavgribkovq@yandex.by>
2021-01-06 23:43:40 +0300

    feat: complited Panel class

--- src/Components/App/index.App.ts
+++ src/Components/App/index.App.ts
@@ -1,6 +1,8 @@
 import LangDictionaries from '../../LangDictionaries/index.langDictionaries';
 import Observer from '../../Observer/index.Observer';
 import { DEFAULT_LANGUAGE } from '../../Constants/index.Constants';
+import Board from './index.Board';
+import Panel from './index.Panel';
 
 export default class App {
   private readonly observer: Observer;
@@ -13,7 +15,9 @@ export default class App {
 
     const parentElem: Element = document.body;
     this.mainElement = document.createElement('main');
-
+    new Panel();
+    // const board = new Board();
+    // board.addHost();
     // this.table = new Table(
     //   this.mainElement,
     //   this.observer,
