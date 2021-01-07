7ada9466ff3b6e22ff3dfac144345faa6d51b356
Kroh Nickolay <375336623761@ya.ru>
2021-01-07 21:15:38 +0300

    feat: add SERVER_URL

--- src/SocketClient/constants.ts
+++ src/SocketClient/constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_URL = '';
+export const SERVER_URL = 'https://rsclone-node-js.herokuapp.com/';
 export const CHAT_EVENT = 'chat message';
 export const CONNECT_EVENT = 'connect';
 export const GREETING_EVENT = 'name';
