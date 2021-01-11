9a8d1ac1bd7ce6229f925dd6f6263f2023cc0141
Kroh Nickolay <375336623761@ya.ru>
2021-01-12 00:12:48 +0300

    feat: add pages templates

---
+++ src/Constants/pages.ts
@@ -0,0 +1,6 @@
+export const LANDING_PAGE = 'landing';
+export const REGISTRATION_PAGE = 'registration';
+export const LOGIN_PAGE = 'login';
+export const SETTINGS_PAGE = 'settings';
+export const GAME_PAGE = 'game';
+export const STATISTICS_PAGE = 'statistics';
---
+++ src/Constants/routes.ts
@@ -0,0 +1,6 @@
+export const LANDING = '/';
+export const REGISTRATION = '/registration';
+export const LOGIN = '/login';
+export const SETTINGS = '/settings';
+export const GAME = '/game';
+export const STATISTICS = '/stat';
