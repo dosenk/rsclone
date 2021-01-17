ee3a2b11e97adb14884f35c24d7266c860399cec
Kroh Nickolay <375336623761@ya.ru>
2021-01-17 17:15:39 +0300

    feat: add SelectWord Component

---
+++ src/Components/SelectWord/constants.ts
@@ -0,0 +1,4 @@
+export const SELECT_CN = 'select_word';
+export const SELECT_CONTENT_CN = `${SELECT_CN}--content`;
+export const SELECT_TITLE_CN = `${SELECT_CN}--title`;
+export const SELECT_BTN_CN = `${SELECT_CN}--btn`;
