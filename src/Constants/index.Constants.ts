db852a33f87270131d505fcd23f57f394666172b
Dmitry Osipenko <eympel@gmail.com>
2021-01-21 14:57:50 +0300

    fix: changed constants socket-server

--- src/Constants/index.Constants.ts
+++ src/Constants/index.Constants.ts
@@ -1,6 +1,5 @@
 export const DEFAULT_LANGUAGE = 'en';
-// export const SOCKET_SERVER: string = 'https://rsclone-node-js.herokuapp.com';
-export const SOCKET_SERVER: string = 'localhost';
+export const SOCKET_SERVER: string = 'https://rsclone-node-js.herokuapp.com';
 export const ROLE_PAINTER: string = 'PAINTER';
 export const ROLE_GUESSER: string = 'GUESSER';
 export const APP_NAME = 'Crocodile';
