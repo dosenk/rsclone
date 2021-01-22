91b355d3048c8b3c7f242fff4fb62bca23b0cb1a
Kroh Nickolay <375336623761@ya.ru>
2021-01-22 23:25:08 +0300

    fix: login page has header

--- src/pages/login.ts
+++ src/pages/login.ts
@@ -1,16 +1,13 @@
 import type Observer from '../Observer/index.Observer';
 import type Router from '../Router/index.Router';
 import Login from '../Components/Login/index.Login';
-import createLayoutElements from '../Components/PageLayout/index.PageLayout';
 
 export default (
   parent: HTMLElement,
   observer: Observer,
   router: Router
 ): void => {
-  const { main, wholeLayout } = createLayoutElements(observer, router);
-  const login = new Login(main, observer, router);
+  const login = new Login(parent, observer, router);
 
   login.start();
-  parent.append(wholeLayout);
 };
