b9a8af0d821d40e9dbbd97ec9ab284e348f91540
Vladislav Gribkov <vladislavgribkovq@yandex.by>
2021-01-23 13:10:06 +0300

    refactor: update style

--- src/pages/statistics.ts
+++ src/pages/statistics.ts
@@ -1,5 +1,3 @@
-import Statistics from '../Components/Statistics/index.Statistics';
-
 export default (parent: HTMLElement): void => {
-  const statistics = new Statistics(parent);
+  parent.append('stat');
 };
