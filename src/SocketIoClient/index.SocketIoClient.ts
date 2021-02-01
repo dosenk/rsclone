dd64f7852d6c86f2ac042c52321d46650f877105
Dmitry Osipenko <eympel@gmail.com>
2021-02-01 18:31:43 +0300

    fix: space between sender and msg

--- src/SocketIoClient/index.SocketIoClient.ts
+++ src/SocketIoClient/index.SocketIoClient.ts
@@ -280,11 +280,6 @@ export default class SocketIoClient {
   }
 
   createForm(): HTMLElement {
-    //     <div class="input-group mb-3">
-    //   <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-    //   <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-    // </div>
-
     const input = createElement('input', FORM_INPUT_CLASS);
     input.setAttribute('name', ANSWER_INPUT);
 
